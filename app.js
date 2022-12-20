6afcf3668eea5d54f5514cf6b99443c789000111
Ishika Goyadi <ishikag@Ishikas-MacBook-Air.local>
2022-12-21 01:56:31 +0530

    second commit

--- app.js
+++ app.js
@@ -3,23 +3,44 @@ const bodyParser=require('body-parser');
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
-var items=["buy food","cook food"];
+app.use(express.static("public"))
+let items=["buy food","cook food"];
+let WorkItem=[];
 app.set('view engine', 'ejs');
 app.get('/',function(req,res){
-    var today= new Date();
-    var option={
+    let today= new Date();
+    let option={
         weekday:"long",
         day:"numeric",
         month:"long"
     };
     var day=today.toLocaleDateString("en-US",option);
-    res.render("list",{dayy:day,newlists:items});
+    res.render("list",{listtitle:day,newlists:items});
 });
 app.post("/",function(req,res){
      var item=req.body.newitem;
-    items.push(item);
-    res.redirect("/");
+     console.log(req.body.list)
+     if(req.body.list==="Work"){
+        WorkItem.push(item);
+        res.redirect("/work")
+     }else{
+        items.push(item);
+        res.redirect("/");
+     }
 })
+
+app.get('/work',function(req,res){
+    res.render("list",{listtitle:"Work List",newlists:WorkItem})
+});
+
+app.post("/work",function(req,res){
+    let item=req.body.newitem;
+    WorkItem.push(item);
+    res.redirect("/work")
+})
+app.get('/about',function(req,res){
+    res.render("about")
+});
 app.listen(1000,function(){
     console.log("server is on port 1000");
 })
